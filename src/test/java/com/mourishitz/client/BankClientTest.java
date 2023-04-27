d825f089d6f21618e838af547a269f4e6815a5fa
Gabriel Morishita <gabrielmorishita@hotmail.com>
2023-04-27 09:47:09 -0300

    � test: creating unit tests

--- src/test/java/com/mourishitz/client/BankClientTest.java
+++ src/test/java/com/mourishitz/client/BankClientTest.java
@@ -3,16 +3,20 @@ package com.mourishitz.client;
 import com.mourishitz.models.Balance;
 import com.mourishitz.models.BalanceCheckRequest;
 import com.mourishitz.models.BankServiceGrpc;
+import com.mourishitz.models.WithdrawRequest;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import java.util.concurrent.CountDownLatch;
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BankClientTest {
 
     private BankServiceGrpc.BankServiceBlockingStub blockingStub;
+    private BankServiceGrpc.BankServiceStub bankServiceStub;
 
     @BeforeAll
     public void setup(){
@@ -21,6 +25,7 @@ public class BankClientTest {
                 .build();
 
         this.blockingStub = BankServiceGrpc.newBlockingStub(managedChannel);
+        this.bankServiceStub = BankServiceGrpc.newStub(managedChannel);
     }
 
     @Test
@@ -31,9 +36,30 @@ public class BankClientTest {
 
         Balance balance = this.blockingStub.getBalance(balanceCheckRequest);
         System.out.println(
-                "Received: " + balance.getAmount()
+                "Balance Amount: " + balance.getAmount()
         );
     }
 
+    @Test
+    public void withdrawTest(){
+        WithdrawRequest withdrawRequest = WithdrawRequest.newBuilder()
+                .setAccountNumber(15)
+                .setAmount(40)
+                .build();
 
+        this.blockingStub.withdraw(withdrawRequest)
+                .forEachRemaining(money -> System.out.println("Withdraw: " + money.getValue()));
+    }
+
+    @Test
+    public void withdrawAsyncTest() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        WithdrawRequest withdrawRequest = WithdrawRequest.newBuilder()
+                .setAccountNumber(10)
+                .setAmount(40)
+                .build();
+
+        this.bankServiceStub.withdraw(withdrawRequest, new MoneyStreamingResponse(latch));
+        latch.await();
+    }
 }
